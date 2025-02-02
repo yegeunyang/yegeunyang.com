5427ec025908fb28d9d8f3b3099dd29acdc8def7
yegeunyang <yegeunyang@icloud.com>
2025-02-02 15:02:36 -0600

    Add side projects section in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,14 @@ export default function Page() {
       <p className="mb-4">
         I&apos;m a senior studying Computer Science and Math at UW-Madison.
       </p>
+
+      <h2 className="mt-8 mb-4 text-xl font-semibold tracking-tighter">Fun side projects</h2>
+      <ul>
+        <li>
+          <a href="https://odin-cv-application-weld.vercel.app" className="underline">Resumé builder</a>
+          <p>A web application for creating resumés, developed with React.js.</p>
+        </li>
+      </ul>
     </section>
   );
 }
